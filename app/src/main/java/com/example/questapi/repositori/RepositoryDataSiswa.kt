d00ed64b45cd04d2f26aee30647cf1badee746eb
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-18 16:31:00 +0700

    Menambahkan implementasi Repository

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -1,9 +1,16 @@
 package com.example.questapi.repositori
 
+import com.example.questapi.apiservice.ServiceApiSiswa
 import com.example.questapi.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-
+    //suspend fun getSatuSiswa(id:Int) : DataSiswa
+    //suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    //suspend fun hapusSatuSiswa(id: Int)::retrofit2.Response<Void>
 }
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+)
