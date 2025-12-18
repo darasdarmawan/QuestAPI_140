549f367e1475dbf25710d1e12a526c309b9b335e
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-18 20:49:39 +0700

    Menambahkan fungsi home screen

---
+++ app/src/main/java/com/example/questapi/view/HalamanHome.kt
@@ -0,0 +1,68 @@
+package com.example.questapi.view
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi.uicontroller.route.DestinasiHome
+import com.example.questapi.viewmodel.EntryViewModel
+import com.example.questapi.viewmodel.HomeViewModel
+import com.example.questapi.viewmodel.provider.PenyediaViewModel
+import com.example.questapi.R
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    //edit 1.1 : tambahkan parameter navigateToItemEntry
+    navigateToItemEntry: () -> Unit,
+    //edit 2.2 : tambahkan parameter navigateToItemUpdate
+    navigateToItemUpdate:(Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection
+        ),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                //edit 1.2 : event onClick
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen
+                    .padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        },
+    ) { innerPadding ->
+        HomeBody(
+            statusUiSiswa = viewModel.listSiswa,
+            onSiswaClick = navigateToItemUpdate,
+            retryAction = viewModel::loadSiswa,
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+        )
+
+    }
+}
