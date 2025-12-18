5ecc838481e75bb25fde4cbb75ac0e21a6b3cb5e
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-18 15:49:25 +0700

    tambah data class detail siswa

--- app/src/main/java/com/example/questapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi/modeldata/DataSiswa.kt
@@ -8,10 +8,17 @@ data class DataSiswa(
     val id : Int,
     val nama : String,
     val alamat : String,
-    val telepon : String
+    val telpon : String
 )
 
 data class UIStateSiswa(
     val detailSiswa : DetailSiswa = DetailSiswa(),
     val isEntryValid : Boolean = false
+)
+
+data class DetailSiswa(
+    val id : Int = 0,
+    val nama : String = "",
+    val alamat: String = "",
+    val telpon: String = ""
 )
