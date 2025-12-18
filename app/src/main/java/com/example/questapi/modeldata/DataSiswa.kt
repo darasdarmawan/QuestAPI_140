68d25c8db9c216be5e79d885d613d4c664d08d3d
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-18 15:45:35 +0700

    tambah data class data siswa

---
+++ app/src/main/java/com/example/questapi/modeldata/DataSiswa.kt
@@ -0,0 +1,12 @@
+package com.example.questapi.modeldata
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telepon : String
+)
