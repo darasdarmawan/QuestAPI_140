4a4086c8c585fed780a3376dc91352829cf55072
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-18 19:18:04 +0700

    tambah fungsi creation extras

---
+++ app/src/main/java/com/example/questapi/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.questapi.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi.repositori.AplikasiDataSiswa
+import com.example.questapi.viewmodel.EntryViewModel
+import com.example.questapi.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel (aplikasiDataSiswa().container
+            .repositoryDataSiswa) }
+        initializer { EntryViewModel (aplikasiDataSiswa().container
+            .repositoryDataSiswa) }
+    }
+}
