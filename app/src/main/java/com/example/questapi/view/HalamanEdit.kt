f78d01e6047146e55f18c70b79b8dd3b4b91a097
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-25 23:40:48 +0700

    Menambahkan fungsi EditSiswaScreen

---
+++ app/src/main/java/com/example/questapi/view/HalamanEdit.kt
@@ -0,0 +1,17 @@
+package com.example.questapi.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi.viewmodel.EditViewModel
+import com.example.questapi.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
