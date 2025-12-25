449eea8fa78b416ede0d984ba58f0a464a16d941
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-25 21:52:01 +0700

    tambah fungsi get status siswa

--- app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,10 @@ package com.example.questapi.apiservice
 
 import com.example.questapi.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {
@@ -13,8 +15,8 @@ interface ServiceApiSiswa {
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
-// @GET("bacaTeman.php/{id}")
-    // suspend fun getSatuSiswa(@Query("id") id: Int) : DataSiswa
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int) : DataSiswa
 
     //@PUT("editTM.php{id}")
     //suspend fun editSatuSiswa(@Query("id") id:Int,@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
