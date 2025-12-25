0876bf60effa1db76f0937682f0cdc47e57bde2b
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-25 21:55:02 +0700

    tambah fungsi edit satu siswa berdasarkan data siswa pd repository data siswa

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -7,9 +7,9 @@ import retrofit2.Response
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    //suspend fun getSatuSiswa(id:Int) : DataSiswa
-    //suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
-    //suspend fun hapusSatuSiswa(id: Int)::retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    //suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
 }
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
@@ -18,4 +18,5 @@ class JaringanRepositoryDataSiswa(
         .getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
     .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id : Int) : DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
