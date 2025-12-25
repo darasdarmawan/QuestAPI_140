6fbe574631da235250cf505b1cae21a0d0dacdc5
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-25 22:20:03 +0700

    Memuat data siswa berdasarkan ID pada EditViewModel

--- app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
@@ -3,11 +3,20 @@ package com.example.questapi.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi.modeldata.UIStateSiswa
 import com.example.questapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel (savedStateHandle: SavedStateHandle, private val respositoryDataSiswa:
 RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init{
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
