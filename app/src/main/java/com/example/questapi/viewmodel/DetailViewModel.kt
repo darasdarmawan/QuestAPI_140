408998ff7045b7c1625e6894550713264c739095
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-25 22:37:07 +0700

    Menambahkan DetailViewModel untuk pengelolaan data detail siswa

--- app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.questapi.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi.modeldata.DataSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+class DetailViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
+RepositoryDataSiswa): ViewModel(){
+
 }
