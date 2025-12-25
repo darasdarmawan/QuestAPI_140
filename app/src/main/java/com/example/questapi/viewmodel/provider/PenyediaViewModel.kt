7b8b18d1ba11ce8fe2516bdc24342b2c41d9226d
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-12-25 22:51:16 +0700

    Menambahkan initializer editviewmodel

--- app/src/main/java/com/example/questapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/provider/PenyediaViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi.repositori.AplikasiDataSiswa
 import com.example.questapi.viewmodel.DetailViewModel
+import com.example.questapi.viewmodel.EditViewModel
 import com.example.questapi.viewmodel.EntryViewModel
 import com.example.questapi.viewmodel.HomeViewModel
 
@@ -20,6 +21,12 @@ object PenyediaViewModel {
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer {
             DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
                 this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
